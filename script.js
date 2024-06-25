564fadff8fe867dd72f59b49c4a2037741e06753
tjennat <theo.jennat98@gmail.com>
2024-06-25 10:44:46 +0200

    adding 4 windows for tuto WIP for images

--- script.js
+++ script.js
@@ -16,12 +16,16 @@ const contents = [
         text: "Each player is assigned a color and receives counters. Each is black on one side and white on the other. Black pawns start the game by placing the first pawn in a location that encloses an opponent's pawn."
     },
     {
-        image: 'tuto-2.png',
-        text: "The player must then flip the opponent's enclosed pawns to their color. This is repeated alternately until the entire board is filled or no more moves can be made."
+        image: 'tuto-2.jpg',
+        text: "To 'enclose' a disc means to surround one or more of your opponent's pieces with two of your own."
     },
     {
-        image: 'tuto-3.png',
-        text: "The game ends when neither player can move. The winner is the player with the most pieces of their color on the board."
+        image: 'tuto-3.jpg',
+        text: "All black pawns between the two white pawns are then flipped over (captured), showing their white side. A piece can capture any number of the opponent's pieces (diagonally, horizontally or vertically)."
+    },
+    {
+        image: 'tuto-4.jpg',
+        text: "If a player cannot enclose at least one of his opponent's pawns, he is forced to skip his turn. When neither player can move any more pieces, the game ends. The winner at the end of the game is the player with the most pieces on the board."
     }
 ];
 
