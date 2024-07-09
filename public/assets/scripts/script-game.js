6bb749db75e50d28ba03ec00dcd96e1a43557525
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-07-09 09:39:35 +0200

    Fix responsive site

--- public/assets/scripts/script-game.js
+++ public/assets/scripts/script-game.js
@@ -33,9 +33,6 @@ createUnityInstance(document.querySelector("#unity-canvas"), {
   productVersion: "0.1",
   // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
   // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
-}).then((unityInstance) => {
-  UnityInstance = unityInstance;
-  sendUserInfoToUnity();
 });
 
 function resizeGame() {
